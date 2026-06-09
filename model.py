a152fe0ce67202bb0fb26397f72bd5b6f6896b94
Sudipto Saha <sudiptosaha5002@gmail.com>
2026-06-10 00:28:16 +0530

    Computed model training successfully

--- model.py
+++ model.py
@@ -10,3 +10,13 @@ df=df.drop('Year',axis=1)
 df_processed=pd.get_dummies(df,drop_first=True)
 print("\n processed data headers:")
 print(df_processed.head(2))
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+X=df_processed.drop('Selling_Price',axis=1)
+y=df_processed['Selling_Price']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=42)
+model=LinearRegression()
+print("\n training the model...")
+model.fit(X_train,y_train)
+print("\n model trained successfully")
